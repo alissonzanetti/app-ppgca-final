872da8fc3fcbd3ca987d40e551b186186c0208f4
Alisson Zanetti <38359615+alissonzanetti@users.noreply.github.com>
2018-05-16 16:21:38 -0300

    Update AppPpgca.js

--- PPGCA/AppPpgca.js
+++ PPGCA/AppPpgca.js
@@ -99,7 +99,7 @@ app.controller('MainController', ['$scope', '$firebaseSimpleLogin', '$location',
                     $location.path('/userHome');
                 }, function(error) {
                     login.loading = false;
-                    $scope.signUpMessage = "Registro do usuário não encontrado";
+                    $scope.signInMessage = "Registro do usuário não encontrado";
                     $rootScope.toggle('myOverlay', 'on');
                     console.log("Usuário e senha não encontrados");
 
